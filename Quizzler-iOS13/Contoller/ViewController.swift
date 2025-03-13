4c0a05d0775759d5e3e5e94e8780f47fa6857180
Ariana Seigh <aseigh646@gmail.com>
2025-03-13 17:12:16 -0400

    3-13 Updates

--- Quizzler-iOS13/Contoller/ViewController.swift
+++ Quizzler-iOS13/Contoller/ViewController.swift
@@ -7,83 +7,112 @@
 //
 //  Updated by Ariana Seigh on 03/12/2025.
 //
+//  Enhancements:
+//  1) 10 sec timer for each question
+//  2) Sound for correct and incorrect answers
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     // Outlets
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    // Connects to struct quiz properties
-    let quiz = [
-        Question(q: "A slug's blood is green.", a: "True"),
-        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-        Question(q: "Google was originally called 'Backrub'.", a: "True"),
-        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-    ]
+    // Connect to QuizBrain model
+    var quizBrain = QuizBrain()
     
-    // Variable to keep track of current question
-    var questionNumber = 0
+    // Variables for timer
+    var timer: Timer?
+    var timeLeft = 10.0
+    
+    // Variables for audio
+    var player: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         updateUI()
-        
+        startTimer()
+    }
+    
+    // Start timer for each question
+    func startTimer() {
+        timeLeft = 10.0
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    // Update timer functon
+    @objc func updateTimer() {
+        if timeLeft > 0 {
+            timeLeft -= 1
+            timerLabel.text = "Time: \(Int(timeLeft)) seconds"
+        }
+        else {
+            timer?.invalidate()
+            quizBrain.nextQuestion()
+            updateUI()
+            startTimer()
+        }
     }
     
     // Trigger for when True or False button is pressed
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
-        let userAnswer = sender.currentTitle  // True, False
-        let actualAnswer = quiz[questionNumber].answer
+        timer?.invalidate()
+        
+        let userAnswer = sender.currentTitle!  // True, False
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
         
         // Change color based on if right or wrong answer
-        if userAnswer == actualAnswer {
+        if userGotItRight {
             sender.backgroundColor = UIColor.green
+            
+            // Audio for correct
+            let url = Bundle.main.url(forResource: "correct", withExtension: "mp3")!
+            player = try! AVAudioPlayer(contentsOf: url)
+            player.play()
         }
         else {
             sender.backgroundColor = UIColor.red
-
+            
+            // Audio for incorrect
+            let url = Bundle.main.url(forResource: "incorrect", withExtension: "mp3")!
+            player = try! AVAudioPlayer(contentsOf: url)
+            player.play()
         }
         
-        // Safety to prevent running error
-        if questionNumber + 1 < quiz.count {
-            questionNumber += 1
-        }
-        else {
-            questionNumber = 0
-        }
+        quizBrain.nextQuestion()
         
         // Delay button color for 0.2 secs
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
         
-        updateUI()
+        startTimer()
     }
     
     // Function that captures functionality (update questionLabel based off current questionNumber) for multiple uses
     @objc func updateUI() {
-        questionLabel.text = quiz[questionNumber].text
+        questionLabel.text = quizBrain.getQuestionText()
+        
+        // Update progress bar as quiz goes on
+        progressBar.progress = quizBrain.getProgress()
+        
+        // Print user's score
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        
+        // Print time left
+        timerLabel.text = "Time: \(Int(timeLeft)) seconds"
         
         // Reset answer button's color for next question
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
-        
-        // Update progress bar as quiz goes on
-        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
     }
 
 }
--- Quizzler-iOS13/Question.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Question.swift
-//  Quizzler-iOS13
-//
-//  Created by Ariana Seigh on 3/12/25.
-//  Copyright © 2025 The App Brewery. All rights reserved.
-//
-
-import Foundation
